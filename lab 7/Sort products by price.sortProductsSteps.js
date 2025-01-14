afec4aa6174e9c8f9debce3d4f01b39a040ef9d1
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-14 09:10:30 +0200

    Create Sort products by price.sortProductsSteps.js

---
+++ lab 7/Sort products by price.sortProductsSteps.js
@@ -0,0 +1,15 @@
+// sortProductsSteps.js
+
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('I am on the homepage', async () => {
+  // Navigate to the homepage
+});
+
+When('I sort products by price', async () => {
+  // Sort products by price
+});
+
+Then('products should be displayed in ascending order of price', async () => {
+  // Verify that products are displayed in ascending order of price
+});
