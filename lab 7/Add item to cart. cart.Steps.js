4103c0d2515ec8d936811b5f1aafa4ff08a7b9d2
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-14 09:04:08 +0200

    Create Add item to cart. cart.Steps.js

---
+++ lab 7/Add item to cart. cart.Steps.js
@@ -0,0 +1,15 @@
+// cartSteps.js
+
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('I am on the product page', async () => {
+  // Navigate to the product page
+});
+
+When('I click on the "Add to Cart" button', async () => {
+  // Click on the "Add to Cart" button
+});
+
+Then('the item should be added to my shopping cart', async () => {
+  // Verify that the item is added to the shopping cart
+});
