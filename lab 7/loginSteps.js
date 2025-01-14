b3353e98a06e280a342222b3cc448fb51e252c4e
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-14 08:57:32 +0200

    Create loginSteps.js

---
+++ lab 7/loginSteps.js
@@ -0,0 +1,19 @@
+// loginSteps.js
+
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('I am on the login page', async () => {
+  // Navigate to the login page
+});
+
+When('I fill in the login form with valid credentials', async () => {
+  // Fill in the login form with valid credentials
+});
+
+When('I submit the login form', async () => {
+  // Submit the login form
+});
+
+Then('I should be logged in successfully', async () => {
+  // Verify that the user is logged in successfully
+});
