1e6e1a1d26e4ef03a306e58cd2ee0b0ede98c02b
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-18 16:28:55 +0200

    Update additional_functions.test.js

--- Lab 1/additional_functions.test.js
+++ Lab 1/additional_functions.test.js
@@ -4,22 +4,22 @@ const { containsArray, containsWord, containsField, executeCallbackOnArray,
 
 //Чи містить масив вказаний об’єкт  
 test('containsArray should return true if array contains the object', () => {
-    expect(containsArray([7, 6, 3], 7)).toBe(true);
+    expect(containsArray([8, 6, 3], 9)).toBe(true);
 });
   
 test('containsArray should return false if array does not contain the object', () => {
-    expect(containsArray([7, 6, 3], 22)).toBe(false);
+    expect(containsArray([8, 6, 3], 24)).toBe(false);
 });
   
 
 
 //Чи містить об’єкт який повертає функція вказане поле
 test('containsWord should return true if string contains the word', () => {
-    expect(containsWord('My name is Kristina', 'Kristina')).toBe(true);
+    expect(containsWord('My name is Maria', 'Maria')).toBe(true);
 });
   
 test('containsWord should return false if string does not contain the word', () => {
-    expect(containsWord('My name is Kristina', 'Mark')).toBe(false);
+    expect(containsWord('My name is Maria', 'Andrey')).toBe(false);
 });
   
 
@@ -27,11 +27,11 @@ test('containsWord should return false if string does not contain the word', () 
 /*Функцію яка приймає масив та зворотній виклик який виконується над елементами масиву.
 Напишіть тест який перевіряє кількість запусків зворотного виклику.*/
 test('containsField should return true if object contains the field', () => {
-    expect(containsField({ name: 'Kristina', age: 21 }, 'name')).toBe(true);
+    expect(containsField({ name: 'Maria', age: 21 }, 'name')).toBe(true);
 });
 
 test('containsField should return false if object does not contain the field', () => {
-    expect(containsField({ name: 'Kristina', age: 21 }, 'gender')).toBe(false);
+    expect(containsField({ name: 'Maria', age: 21 }, 'gender')).toBe(false);
 });
   
 
@@ -47,8 +47,8 @@ test('executeCallbackOnArray should call callback for each array element', () =>
   
 test('executeCallbackOnPhrase should call callback with array of words', () => {
     const callback = jest.fn();
-    executeCallbackOnPhrase('hello Kris', callback);
-    expect(callback).toHaveBeenCalledWith(['hello', 'Kris']);
+    executeCallbackOnPhrase('hello Mary', callback);
+    expect(callback).toHaveBeenCalledWith(['hello', 'Mary']);
 });
   
 
