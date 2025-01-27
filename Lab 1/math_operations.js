6183d9cde0bc1bb6526a17501fac3f4242684da9
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-27 09:23:42 +0200

    Update math_operations.js

--- Lab 1/math_operations.js
+++ Lab 1/math_operations.js
@@ -1,23 +1,24 @@
-function add(a, b) 
-{
+// Функція додавання
+function add(a, b) {
     return a + b;
 }
-  
-function subtract(a, b) 
-  {
+
+// Функція віднімання
+function subtract(a, b) {
     return a - b;
 }
-  
-function multiply(a, b) 
-  {
+
+// Функція множення
+function multiply(a, b) {
     return a * b;
 }
-  
-function divide(a, b) 
-{
-    if (b === 0) throw new Error('Division by zero');
+
+// Функція ділення
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error('Division by zero is not allowed');
+    }
     return a / b;
 }
-  
+
 module.exports = { add, subtract, multiply, divide };
-  
