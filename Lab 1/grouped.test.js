3e4b50c7fbcd62a77ae459e5d727e1eeb0859e82
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-27 09:23:09 +0200

    Update grouped.test.js

--- Lab 1/grouped.test.js
+++ Lab 1/grouped.test.js
@@ -1,43 +1,32 @@
-onst { sumArray, filterPositive, filterNegative } = require('./array_utils');
-const { isPalindrome, isAnagram } = require('./string_operations');
+const { describe, test } = require('@jest/globals');
 
-//Тести для файлу array_utils.js
-describe('array_utils test', () => {
-  let testArray;
+describe('Tests for array_utils.js', () => {
+    const { calculateSum, filterPositiveNumbers, filterNegativeNumbers } = require('./array_utils');
 
-  beforeAll(() => {
-    testArray = [1, -2, 3, -4, 5];
-  });
+    test('calculateSum should return the correct sum of array elements', () => {
+        const array = [1, 2, 3, 4, 5];
+        expect(calculateSum(array)).toBe(15);
+    });
 
-  test('sumArray should return sum of array elements', () => {
-    expect(sumArray(testArray)).toBe(3);
-  });
+    test('filterPositiveNumbers should return an array containing only positive numbers', () => {
+        const array = [-1, 2, -3, 4, -5];
+        expect(filterPositiveNumbers(array)).toEqual([2, 4]);
+    });
 
-  test('filterPositive should return array of positive numbers', () => {
-    expect(filterPositive(testArray)).toEqual([1, 3, 5]);
-  });
-
-  test('filterNegative should return array of negative numbers', () => {
-    expect(filterNegative(testArray)).toEqual([-2, -4]);
-  });
+    test('filterNegativeNumbers should return an array containing only negative numbers', () => {
+        const array = [-1, 2, -3, 4, -5];
+        expect(filterNegativeNumbers(array)).toEqual([-1, -3, -5]);
+    });
 });
 
+describe('Tests for string_operations.js', () => {
+    const { isPalindrome, isAnagram } = require('./string_operations');
 
-//Тести для файлу string_operations.test.js 
-describe('string_operations test', () => {
-  test('isPalindrome should return true for a palindrome', () => {
-    expect(isPalindrome('No lemon, no melon')).toBe(true);
-  });
+    test('isPalindrome should return true for a palindrome string', () => {
+        expect(isPalindrome('radar')).toBe(true);
+    });
 
-  test('isPalindrome should return false for a non-palindrome', () => {
-    expect(isPalindrome('Christina')).toBe(false);
-  });
-
-  test('isAnagram should return true for anagrams', () => {
-    expect(isAnagram('cinema', 'iceman')).toBe(true);
-  });
-
-  test('isAnagram should return false for non-anagrams', () => {
-    expect(isAnagram('world', 'hello')).toBe(false);
-  });
+    test('isAnagram should return true for anagram strings', () => {
+        expect(isAnagram('listen', 'silent')).toBe(true);
+    });
 });
