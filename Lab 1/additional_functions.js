0f94d464912dd21573215b9a0b66fa93569dab09
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-27 09:18:16 +0200

    Update additional_functions.js

--- Lab 1/additional_functions.js
+++ Lab 1/additional_functions.js
@@ -1,51 +1,45 @@
-//Чи містить масив вказаний об’єкт
-function containsArray(arr, obj) 
-{
-    return arr.includes(obj);
-}
-  
-//Чи містить текстовий фрагмент вказане слово
-function containsWord(str, word) 
-{
-    return str.includes(word);
+// Функція, яка перевіряє, чи містить масив вказаний об’єкт
+function arrayContainsObject(array, object) {
+    return array.some(item => JSON.stringify(item) === JSON.stringify(object));
 }
 
-//Чи містить об’єкт який повертає функція вказане поле
-function containsField(obj, field) 
-{
-    return obj.hasOwnProperty(field);
+// Функція, яка перевіряє, чи містить текстовий фрагмент вказане слово
+function textContainsWord(text, word) {
+    const regex = new RegExp(`\\b${word}\\b`, 'i');
+    return regex.test(text);
 }
- 
-/*Функцію яка приймає масив та зворотній виклик який виконується над елементами масиву.
-Напишіть тест який перевіряє кількість запусків зворотного виклику.*/
-function executeCallbackOnArray(arr, callback) 
-{
-    arr.forEach(callback);
+
+// Функція, яка перевіряє, чи містить об’єкт, який повертає функція, вказане поле
+function objectReturnedByFunctionHasField(func, field) {
+    const obj = func();
+    return Object.prototype.hasOwnProperty.call(obj, field);
 }
- 
-/*Функція яка приймає текстовий рядок із фразою та зворотній виклик. Функція перетворює
-фразу на масив слів передає її у зворотній виклик. Протестуйте з якими аргументами
-викликається зворотній виклик.*/
-function executeCallbackOnPhrase(phrase, callback) 
-{
+
+// Функція, яка приймає масив та зворотній виклик і виконує цей зворотній виклик над елементами масиву
+function applyCallbackToArray(array, callback) {
+    array.forEach(callback);
+}
+
+// Функція, яка приймає текстовий рядок і зворотній виклик, перетворює фразу на масив слів та передає його у зворотній виклик
+function phraseToArrayAndApplyCallback(phrase, callback) {
     const words = phrase.split(' ');
     callback(words);
 }
-  
-/*Створіть об’єкт який містить поле імені та поле прізвища, додайте в об’єкт метод introduce
-який виводить у консоль фразу «Hello, I’m firstname lastname». З використання jest.spyOn
-преревірте кількість викликів методу.*/
-function createPerson(firstName, lastName) 
-{
-    return {
-      firstName,
-      lastName,
-      introduce() {
-        console.log(`Hello, I’m ${this.firstName} ${this.lastName}`);
-      }
-    };
-}
-  
-module.exports = { containsArray, containsWord, containsField, 
-    executeCallbackOnArray, executeCallbackOnPhrase,createPerson };
-  
+
+// Об’єкт, який містить поле імені та поле прізвища, доданий метод introduce
+const person = {
+    firstName: 'Firstname',
+    lastName: 'Lastname',
+    introduce() {
+        console.log(`Hello, I'm ${this.firstName} ${this.lastName}`);
+    }
+};
+
+module.exports = {
+    arrayContainsObject,
+    textContainsWord,
+    objectReturnedByFunctionHasField,
+    applyCallbackToArray,
+    phraseToArrayAndApplyCallback,
+    person
+};
