f823b58018b759adc18694c564212733d8bbaad1
MariaSkachkova <skackovamaria945@gmail.com>
2025-01-27 09:24:48 +0200

    Update math_operations_params.test.js

--- Lab 1/math_operations_params.test.js
+++ Lab 1/math_operations_params.test.js
@@ -1,33 +1,52 @@
 const { add, subtract, multiply, divide } = require('./math_operations');
 
+// Параметризовані тести для функції додавання
 test.each([
-  [20, 17, 37],
-  [-37, 37, 0],
-  [15.5, 25.2, 40.7]
-])('add(%i, %i) should return %i', (a, b, expected) => {
-  expect(add(a, b)).toBe(expected);
+    [2, 3, 5],
+    [-2, -3, -5],
+    [0, 0, 0],
+    [10, -5, 5],
+    [2.5, 3.5, 6],
+])('adds %i + %i to equal %i', (a, b, expected) => {
+    expect(add(a, b)).toBe(expected);
 });
 
+// Параметризовані тести для функції віднімання
 test.each([
-  [5.8, 3.4, 2.4],
-  [3, -10.5, 13.5],
-  [10, 27.9, -17.9]
-])('subtract(%i, %i) should return %i', (a, b, expected) => {
-  expect(subtract(a, b)).toBe(expected);
+    [5, 2, 3],
+    [-5, -2, -3],
+    [0, 0, 0],
+    [10, 5, 5],
+    [2.5, 1.5, 1],
+])('subtracts %i - %i to equal %i', (a, b, expected) => {
+    expect(subtract(a, b)).toBe(expected);
 });
 
+// Параметризовані тести для функції множення
 test.each([
-  [2.2, 3.3, 7.26],
-  [2.5, -3, -7.5],
-  [12.3, 7.5, 92.25]
-])('multiply(%i, %i) should return %i', (a, b, expected) => {
-  expect(multiply(a, b)).toBe(expected);
+    [2, 3, 6],
+    [-2, -3, 6],
+    [0, 5, 0],
+    [10, -5, -50],
+    [2.5, 2, 5],
+])('multiplies %i * %i to equal %i', (a, b, expected) => {
+    expect(multiply(a, b)).toBe(expected);
 });
 
+// Параметризовані тести для функції ділення
 test.each([
-  [6.6, 3.3, 2],
-  [5.5, -5.5, -1],
-  [36.9, 9, 4.1]
-])('divide(%i, %i) should return %i', (a, b, expected) => {
-  expect(divide(a, b)).toBe(expected);
+    [10, 2, 5],
+    [-10, -2, 5],
+    [0, 5, 0],
+    [10, -5, -2],
+    [2.5, 0.5, 5],
+])('divides %i / %i to equal %i', (a, b, expected) => {
+    expect(divide(a, b)).toBe(expected);
+});
+
+// Тест для перевірки ділення на нуль
+test('divides by zero throws an error', () => {
+    expect(() => {
+        divide(10, 0);
+    }).toThrow();
 });
